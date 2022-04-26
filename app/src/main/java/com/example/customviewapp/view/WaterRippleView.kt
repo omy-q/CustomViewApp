949448945f2863d6a2c583db691d2427052d2f37
omy-q <olya_aparina@mail.ru>
2022-04-26 20:30:13 +0300

    create custom view

---
+++ app/src/main/java/com/example/customviewapp/view/WaterRippleView.kt
@@ -0,0 +1,13 @@
+package com.example.customviewapp.view
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+class WaterRippleView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null
+) : View(context, attrs) {
+
+
+}
