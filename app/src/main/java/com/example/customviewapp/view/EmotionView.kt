8182dc06b20c7fb5ef81cb661dd6b7b9e27d78b1
omy-q <olya_aparina@mail.ru>
2022-04-26 19:56:13 +0300

    create custom attrs, add to emotion view and create winks eyes

--- app/src/main/java/com/example/customviewapp/view/EmotionView.kt
+++ app/src/main/java/com/example/customviewapp/view/EmotionView.kt
@@ -1,15 +1,11 @@
 package com.example.customviewapp.view
 
 import android.content.Context
-import android.graphics.Canvas
-import android.graphics.Paint
-import android.graphics.Path
-import android.graphics.RectF
+import android.graphics.*
 import android.util.AttributeSet
 import android.view.View
 import androidx.core.content.ContextCompat
 import com.example.customviewapp.R
-import com.example.customviewapp.dp
 
 class EmotionView @JvmOverloads constructor(
     context: Context,
@@ -17,16 +13,50 @@ class EmotionView @JvmOverloads constructor(
 ) : View(context, attrs) {
 
     companion object {
-        private const val faceColor = R.color.rich_yellow
-        private const val eyesColor = R.color.dark_blue
-        private const val mouthColor = R.color.red
-        private const val borderColor = R.color.black
+        private const val DEFAULT_FACE_COLOR = Color.YELLOW
+        private const val DEFAULT_EYES_COLOR = Color.BLUE
+        private const val DEFAULT_MOUTH_COLOR = Color.RED
+        private const val DEFAULT_BORDER_COLOR = Color.BLACK
+        private const val DEFAULT_BORDER_VALUE = 4.0f
 
-        private val borderSize = 2.dp()
+        const val NO_WINKS = 0
+        const val WINKS_LEFT_EYE = 1
+        const val WINKS_RIGHT_EYE = 2
     }
 
+    private var faceColor = DEFAULT_FACE_COLOR
+    private var eyesColor = DEFAULT_EYES_COLOR
+    private var mouthColor = DEFAULT_MOUTH_COLOR
+    private var borderColor = DEFAULT_BORDER_COLOR
+    private var borderSize = DEFAULT_BORDER_VALUE
+
     private val paint = Paint()
-    private val path = Path()
+    private val eyesPath = Path()
+    private val mouthPath = Path()
+
+    var emotionState = NO_WINKS
+        set(value) {
+            field = value
+            invalidate()
+        }
+
+    init {
+        setupAttributes(attrs)
+    }
+
+    private fun setupAttributes(attrs: AttributeSet?) {
+        if (attrs != null) {
+            val typedArray = context.obtainStyledAttributes(attrs, R.styleable.EmotionView)
+            faceColor = typedArray.getColor(R.styleable.EmotionView_faceColor, DEFAULT_FACE_COLOR)
+            eyesColor = typedArray.getColor(R.styleable.EmotionView_eyesColor, DEFAULT_EYES_COLOR)
+            mouthColor = typedArray.getColor(R.styleable.EmotionView_mouthColor, DEFAULT_MOUTH_COLOR)
+            borderColor = typedArray.getColor(R.styleable.EmotionView_borderColor, DEFAULT_BORDER_COLOR)
+            borderSize = typedArray.getDimension(R.styleable.EmotionView_borderSize, DEFAULT_BORDER_VALUE)
+            emotionState = typedArray.getInt(R.styleable.EmotionView_emotionState, NO_WINKS)
+
+            typedArray.recycle()
+        }
+    }
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
@@ -37,7 +67,7 @@ class EmotionView @JvmOverloads constructor(
 
     private fun drawFace(canvas: Canvas) {
         paint.style = Paint.Style.FILL
-        paint.color = ContextCompat.getColor(context, faceColor)
+        paint.color = faceColor
         canvas.drawCircle(
             (width / 2 + paddingStart).toFloat(),
             (height / 2 + paddingTop).toFloat(),
@@ -46,8 +76,8 @@ class EmotionView @JvmOverloads constructor(
         )
 
         paint.style = Paint.Style.STROKE
-        paint.color = ContextCompat.getColor(context, borderColor)
-        paint.strokeWidth = borderSize.toFloat()
+        paint.color = borderColor
+        paint.strokeWidth = borderSize
         canvas.drawCircle(
             (width / 2).toFloat(),
             (height / 2).toFloat(),
@@ -57,35 +87,102 @@ class EmotionView @JvmOverloads constructor(
     }
 
     private fun drawEyes(canvas: Canvas) {
-        val leftEye = RectF(
-            (4 * width / 16).toFloat(),
-            (4 * height / 16).toFloat(),
-            (6 * width / 16).toFloat(),
-            (7 * height / 16).toFloat(),
-        )
-        val rightEye = RectF(
-            (10 * width / 16).toFloat(),
-            (4 * height / 16).toFloat(),
-            (12 * width / 16).toFloat(),
-            (7 * height / 16).toFloat(),
-        )
+        eyesPath.reset()
+        paint.reset()
+        when (emotionState) {
+            NO_WINKS -> {
+                val leftEye = RectF(
+                    (4 * width / 16).toFloat(),
+                    (4 * height / 16).toFloat(),
+                    (6 * width / 16).toFloat(),
+                    (7 * height / 16).toFloat(),
+                )
+                val rightEye = RectF(
+                    (10 * width / 16).toFloat(),
+                    (4 * height / 16).toFloat(),
+                    (12 * width / 16).toFloat(),
+                    (7 * height / 16).toFloat(),
+                )
+                paint.style = Paint.Style.FILL
+                paint.color = eyesColor
+                canvas.drawOval(leftEye, paint)
+                canvas.drawOval(rightEye, paint)
 
-        paint.style = Paint.Style.FILL
-        paint.color = ContextCompat.getColor(context, eyesColor)
-        canvas.drawOval(leftEye, paint)
-        canvas.drawOval(rightEye, paint)
+                paint.style = Paint.Style.STROKE
+                paint.color = borderColor
+                paint.strokeWidth = borderSize
+                canvas.drawOval(leftEye, paint)
+                canvas.drawOval(rightEye, paint)
+            }
+            WINKS_LEFT_EYE -> {
+                eyesPath.moveTo((4 * width / 16).toFloat(), (6 * height / 16).toFloat())
+                eyesPath.quadTo(
+                    (5 * width / 16).toFloat(),
+                    (4 * height / 16).toFloat(),
+                    (6 * width / 16).toFloat(),
+                    (6 * height / 16).toFloat(),
+                )
+                eyesPath.quadTo(
+                    (5 * width / 16).toFloat(),
+                    (5 * height / 16).toFloat(),
+                    (4 * width / 16).toFloat(),
+                    (6 * height / 16).toFloat(),
+                )
+                val rightEye = RectF(
+                    (10 * width / 16).toFloat(),
+                    (4 * height / 16).toFloat(),
+                    (12 * width / 16).toFloat(),
+                    (7 * height / 16).toFloat(),
+                )
+                paint.style = Paint.Style.FILL
+                paint.color = eyesColor
+                canvas.drawOval(rightEye, paint)
+                paint.color = borderColor
+                canvas.drawPath(eyesPath, paint)
 
-        paint.style = Paint.Style.STROKE
-        paint.color = ContextCompat.getColor(context, borderColor)
-        paint.strokeWidth = borderSize.toFloat()
-        canvas.drawOval(leftEye, paint)
-        canvas.drawOval(rightEye, paint)
+                paint.style = Paint.Style.STROKE
+                paint.color = borderColor
+                paint.strokeWidth = borderSize
+                canvas.drawOval(rightEye, paint)
+            }
+            WINKS_RIGHT_EYE -> {
+                val leftEye = RectF(
+                    (4 * width / 16).toFloat(),
+                    (4 * height / 16).toFloat(),
+                    (6 * width / 16).toFloat(),
+                    (7 * height / 16).toFloat(),
+                )
+                eyesPath.moveTo((10 * width / 16).toFloat(), (6 * height / 16).toFloat())
+                eyesPath.quadTo(
+                    (11 * width / 16).toFloat(),
+                    (4 * height / 16).toFloat(),
+                    (12 * width / 16).toFloat(),
+                    (6 * height / 16).toFloat(),
+                )
+                eyesPath.quadTo(
+                    (11 * width / 16).toFloat(),
+                    (5 * height / 16).toFloat(),
+                    (10 * width / 16).toFloat(),
+                    (6 * height / 16).toFloat(),
+                )
+                paint.style = Paint.Style.FILL
+                paint.color = eyesColor
+                canvas.drawOval(leftEye, paint)
+                paint.color = borderColor
+                canvas.drawPath(eyesPath, paint)
+
+                paint.style = Paint.Style.STROKE
+                paint.color = borderColor
+                paint.strokeWidth = borderSize
+                canvas.drawOval(leftEye, paint)
+            }
+        }
     }
 
     private fun drawMouth(canvas: Canvas) {
-        path.moveTo((3 * width / 16).toFloat(), (10 * height / 16).toFloat())
-        path.lineTo((13 * width / 16).toFloat(), (10 * height / 16).toFloat())
-        path.quadTo(
+        mouthPath.moveTo((3 * width / 16).toFloat(), (10 * height / 16).toFloat())
+        mouthPath.lineTo((13 * width / 16).toFloat(), (10 * height / 16).toFloat())
+        mouthPath.quadTo(
             (width / 2).toFloat(),
             (height + 2 * height / 16).toFloat(),
             (3 * width / 16).toFloat(),
@@ -93,12 +190,12 @@ class EmotionView @JvmOverloads constructor(
         )
 
         paint.style = Paint.Style.FILL
-        paint.color = ContextCompat.getColor(context, mouthColor)
-        canvas.drawPath(path, paint)
+        paint.color = mouthColor
+        canvas.drawPath(mouthPath, paint)
 
         paint.style = Paint.Style.STROKE
-        paint.color = ContextCompat.getColor(context, borderColor)
-        paint.strokeWidth = borderSize.toFloat()
-        canvas.drawPath(path, paint)
+        paint.color = borderColor
+        paint.strokeWidth = borderSize
+        canvas.drawPath(mouthPath, paint)
     }
 }
