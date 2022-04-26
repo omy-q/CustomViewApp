0765787d2f65e876d948ef7c876a3fe106dc198a
omy-q <olya_aparina@mail.ru>
2022-04-26 20:03:48 +0300

    set click listener for custom view for change emotion state

--- app/src/main/java/com/example/customviewapp/MainActivity.kt
+++ app/src/main/java/com/example/customviewapp/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.customviewapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.customviewapp.view.EmotionView
 
 class MainActivity : AppCompatActivity() {
+    private var emotionView: EmotionView? = null
+    private var clickCount = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initView()
+    }
+
+    private fun initView() {
+        emotionView = findViewById(R.id.emotion_view)
+        emotionView?.setOnClickListener {
+            clickCount ++
+            if (clickCount > 2) {
+                clickCount = 0
+            }
+            emotionView?.emotionState = clickCount
+        }
     }
 }
